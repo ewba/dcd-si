949b450829880440564ed629c049f167f33ea382
Jaka Kranjc <smgl@lynxlynx.info>
2024-01-08 00:20:26 +0100

    scrollIntoViewIfNeeded can be satisfactorily emulated by the standard method

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ function updatemenu() {
   }
 }
 
-// carousel vertical scroll prevention based on Markus Oberlehner's snippet (but doesn't work on ff due to missing scrollIntoViewIfNeeded)
+// carousel vertical scroll prevention based on Markus Oberlehner's snippet
 window.addEventListener("load", (ev) => {
   let els = document.querySelectorAll(".carousel__navigation, .carousel__prev, .carousel__next");
   for (let el of els) {
@@ -16,13 +16,8 @@ window.addEventListener("load", (ev) => {
       const $slide = document.querySelector(event.target.getAttribute('href'));
       if (!$slide) return;
 
-      if ($slide.scrollIntoViewIfNeeded) {
-        event.preventDefault();
-        $slide.scrollIntoViewIfNeeded();
-      } else if ($slide.scrollIntoView) {
-        event.preventDefault();
-        $slide.scrollIntoView();
-      }
+      event.preventDefault();
+      $slide.scrollIntoView({ block: "nearest" });
     });
   }
 });
